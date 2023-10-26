260324e9f91e02ac88efdcf6f42e85f2f5cdaab4
yanquielarango <yanquiel.arango@gmail.com>
2023-10-26 12:31:41 +0200

    creating a chart

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,102 @@
 <script setup>
 import { ref, shallowRef, computed, watch, nextTick} from 'vue'
-impor
+import Chart from 'chart.js/auto'
+
+
+const weights = ref([])
+
+const weightChartEl = ref(null)
+
+const weightChart = shallowRef(null)
+
+const weightInput = ref(60.0)
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0}
+})
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value ,
+    date: new Date().getTime()
+  })
+}
+
+watch(weights, newWeights => {
+  const ws = [...newWeights]
+
+  nextTick(() => {
+    weightChart.value = new Chart(weightChartEl.value.getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: ws 
+              .sort((a,b) => a.date - b.date)
+              .map(w => new Date(w.date).toLocaleDateString()),
+        datasets: [
+          {
+            label: 'Weight',
+            data: ws
+                .sort((a,b) => a.date - b.date)
+                .map(w => w.weight),
+            backgroundColor: 'rgba(225, 105, 180, 0.2)',
+            borderColor: 'rgba(225, 105, 180)',
+            borderWidth: 1,
+            fill: true
+          }
+        ]    
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    })
+  })
+  
+}, {deep: true})
+
 </script>
 
 <template>
-  <h1> Holla</h1>
+  <main>
+
+    <h1>Weight Tracker</h1>
+
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Current weight (kg)</small>
+    </div>
+
+    <form @submit.prevent="addWeight">
+      <input type="number" step="0.1" v-model="weightInput"/>
+      <input type="submit" value="Add weight" />
+    </form>
+    
+    <div v-if="weights && weights.length > 0">
+      <h2>Last 7 days</h2>
+
+      <div class="canvas-box">
+        <canvas ref="weightChartEl"></canvas>
+      </div>
+
+      <div class="weight-history">
+        <h2>Weight History</h2>
+        <ul>
+
+          <li v-for="weight in weights" :key="weight.date">
+            <span>
+              {{ weight.weight }} kg
+            </span>
+            <small>
+              {{ new Date(weight.date).toLocaleDateString() }}
+            </small>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+
+   
+
+  </main>
+
 </template>
